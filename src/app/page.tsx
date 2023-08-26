320c8f82ea77c295a690254989a149ad2b7c0caa
Shubham <shubham@Shubhams-MacBook-Pro.local>
2023-08-26 23:48:04 +0530

    Project Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/componants/footer";
+import Header from "@/componants/header";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Header />
+      <Footer />
+    </main>
+  );
+}
