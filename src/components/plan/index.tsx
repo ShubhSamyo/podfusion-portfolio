5c6838f246e6b4bf7f7876ba3ead450fc09aaa81
Shubham <shubham@Shubhams-MacBook-Pro.local>
2023-09-01 23:58:47 +0530

    plans

--- src/components/plan/index.tsx
+++ src/components/plan/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
 import "./plan.css";
-import Button from "../button";
 import PlanCard from "./plancard";
 
 const Plan = () => {
   return (
-    <section className="container-fluid">
+    <section className="container-fluid plan_image">
       <div className="container">
+        <div className="tw-text-center tw-text-7xl	aclonica white-color">
+          Choose the Plan that Fits
+          <br />
+          Your Business
+        </div>
+        <div className="tw-flex  tw-justify-center roboto-serif liberty-color tw-text-3xl">
+          Lorem ipsum dolor sit amet consectetur. Id
+          <br />
+          vitae semper sed risus.
+        </div>
+
         <div className="row ">
           <PlanCard />
           <PlanCard />
